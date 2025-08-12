052395fa4dbf218846b290fce6d59b556699b9f6
Manish <manishguhe301@gmail.com>
2025-08-12 08:09:58 +0530

    fix: pdf link, footer to use year, twitter added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 export default function Footer() {
+  const date = new Date()
   return (
     <footer className='mb-10 px-4 text-center text-gray-500'>
       <small className='mb-2 block text-xs'>
-        &copy; 2025 Manish Guhe. All rights reserved.
+        &copy; {date.getFullYear()} Manish Guhe. All rights reserved.
       </small>
       <p className='text-xs'>
         <span className='font-semibold'>About this website:</span> built with
--- components/Intro.tsx
+++ components/Intro.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
 import { HiDownload } from 'react-icons/hi';
 import { FaGithubSquare } from 'react-icons/fa';
+import { FaXTwitter } from "react-icons/fa6";
 import { useSectionInView } from '@/lib/hooks';
 import { useActiveSectionContext } from '@/context/active-section-context';
 
@@ -91,11 +92,19 @@ export default function Intro() {
 
         <a
           className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10'
-          href='/Resume-Manish Anilrao Guhe.pdf'
+          href='/Resume-Manish Anilrao Guhe-new.pdf'
           download
         >
           Download CV{' '}
           <HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
+        </a>
+
+        <a
+          className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'
+          href='https://x.com/manish_guhe818'
+          target='_blank'
+        >
+          <FaXTwitter />
         </a>
 
         <a
