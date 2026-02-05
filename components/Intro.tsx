99180f0f3aa574e37d97a40c42b00c66062ecfb8
Manish <manishguhe301@gmail.com>
2026-02-05 20:54:44 +0530

    chore(resume): update intro experience and refresh PDF details
    Bump displayed experience to 2 years and revise resume PDF with updated internship dates, clearer role descriptions, and corrected certification/Office wording for accuracy and consistency.

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -65,7 +65,7 @@ export default function Intro() {
       >
         <span className='font-bold'>Hello, I&apos;m Manish.</span> I&apos;m a{' '}
         <span className='font-bold'>frontend developer</span> with{' '}
-        <span className='font-bold'>1.5 years</span> of experience. I enjoy
+        <span className='font-bold'>2 years</span> of experience. I enjoy
         building <span className='italic'>sites & apps</span>. My focus is{' '}
         <span className='underline'>React (Next.js)</span>.
       </motion.h1>
