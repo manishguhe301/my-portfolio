9ae78105820ab369bbe1c7e48902c4579cec8de2
Manish <manishguhe301@gmail.com>
2026-04-28 14:35:15 +0530

    feat(project): remove status badge from project card

--- components/Project.tsx
+++ components/Project.tsx
@@ -37,22 +37,6 @@ export default function Project({
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[52%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <div className="flex items-center gap-3">
             <h3 className="text-2xl font-semibold">{title}</h3>
-
-            {status && (
-              <span
-                className={`
-        text-[0.65rem] px-3 py-1 rounded-full uppercase tracking-wider font-medium
-        ${status === "Completed"
-                    ? "bg-emerald-500/15 text-emerald-600 dark:text-emerald-400"
-                    : status === "In Progress"
-                      ? "bg-yellow-500/15 text-yellow-600 dark:text-yellow-400"
-                      : "bg-slate-500/15 text-slate-600 dark:text-slate-400"
-                  }
-      `}
-              >
-                {status}
-              </span>
-            )}
           </div>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
